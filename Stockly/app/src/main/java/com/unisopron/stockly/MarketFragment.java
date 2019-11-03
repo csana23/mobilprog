42d03b960949d40a2abe3f0704ba8f243d47134e
Csanaki Richárd <richard.csanaki@gmail.com>
2019-11-03 20:03:32 +0100

    shit works

--- Stockly/app/src/main/java/com/unisopron/stockly/MarketFragment.java
+++ Stockly/app/src/main/java/com/unisopron/stockly/MarketFragment.java
@@ -1,6 +1,7 @@
 package com.unisopron.stockly;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +11,73 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.components.YAxis.AxisDependency;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MarketFragment extends Fragment {
+    //implements OnSeekBarChangeListener, OnChartValueSelectedListener
+
+    private LineChart chart;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_market, container, false);
+        View view =inflater.inflate(R.layout.fragment_market, container, false);
 
+        chart = view.findViewById(R.id.chart);
 
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 50));
+        values.add(new Entry(2, 100));
+
+        LineDataSet set1;
+
+        // axes
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(11f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+
+        if (chart.getData() != null && chart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
+        } else {
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setAxisDependency(AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.WHITE);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(65);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setDrawCircleHole(false);
+
+            LineData data = new LineData(set1);
+
+            data.setValueTextColor(Color.WHITE);
+            data.setValueTextSize(9f);
+
+            chart.setData(data);
+        }
 
         return view;
     }
