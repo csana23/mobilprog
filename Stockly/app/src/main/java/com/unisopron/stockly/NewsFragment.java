99625cca8070e12b11de34c1bd8c6f9ceba61bea
Csanaki Richárd <richard.csanaki@gmail.com>
2019-11-03 21:09:24 +0100

    adding http capabilities

--- Stockly/app/src/main/java/com/unisopron/stockly/NewsFragment.java
+++ Stockly/app/src/main/java/com/unisopron/stockly/NewsFragment.java
@@ -4,16 +4,68 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsFragment extends Fragment {
+
+    private TextView textViewResult;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        textViewResult = view.findViewById(R.id.text_view_result);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.alphavantage.co")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AlphaVantageApi alphaVantageApi = retrofit.create(AlphaVantageApi.class);
+
+        Call<List<Post>> call = alphaVantageApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                for (Post post : posts) {
+                    String content = "";
+                    content += "Symbol: " + post.getSymbol() + "\n";
+                    content += "Date: " + post.getDate() + "\n";
+                    content += "Close: " + post.getClose() + "\n";
+                    content += "Volume: " + post.getVolume() + "\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
+        return view;
     }
 }
