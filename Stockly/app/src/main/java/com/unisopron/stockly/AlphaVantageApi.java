484f94fbe46fe242f31fec051d185f0ee545a4f6
Csanaki Richárd <richard.csanaki@gmail.com>
2019-11-27 21:19:37 +0100

    hmmm

--- Stockly/app/src/main/java/com/unisopron/stockly/AlphaVantageApi.java
+++ Stockly/app/src/main/java/com/unisopron/stockly/AlphaVantageApi.java
@@ -3,12 +3,11 @@ package com.unisopron.stockly;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Url;
 
 public interface AlphaVantageApi {
 
-    @GET("{path}")
-    Call<ResponseBody> getResponse(@Path("path") String path);
+    @GET
+    Call<ResponseBody> getResponse(@Url String url);
 
 }
